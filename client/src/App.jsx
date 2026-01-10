f2f8d920fb1da58f5f190a27f147121aa88c2453
Aniket-bit7 <aniketpathak1073@gmail.com>
2026-01-11 02:09:48 +0530

    resolving error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Layout from "./pages/Layout";
 import Dashboard from "./pages/Dashboard";
@@ -10,7 +9,6 @@ const App = () => {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/layout" element={<Layout />}>
           <Route index element={<Dashboard />} />
